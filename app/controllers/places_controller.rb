e0032fad55cd443c5e0729cca836b35d4b860bdb
Tanya Goley <tanya.goley@kellogg.northwestern.edu>
2025-02-26 17:15:31 +0000

    Added places

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,29 @@
+class PlacesController < ApplicationController
+    
+    def index
+      @places = Place.all
+    end
+  
+    def show
+      @place = Place.find_by("id" => params["id"])
+      @entries = Entry.where("place_id" => params["id"])
+    end
+  
+    def new
+    end
+    
+    def create
+      # start with a new Place
+      @place = Place.new
+  
+      # assign user-entered form data to Place's columns
+      @place["name"] = params["name"]
+  
+      # save Contact row
+      @place.save
+  
+      # redirect user
+      redirect_to "/"
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+
+  # Home page show the list of places
+  get("/", { :controller => "places", :action => "index" })
+
+  # Routes for Places
+  get("/places/new", { :controller => "places", :action => "new" }) # Form for new place
+  post("/places/new", { :controller => "places", :action => "create" }) # Process new place
+  get("/places/:id", { :controller => "places", :action => "show" }) # Show a single place
+
+  # Routes for Entries (associated with a place)
+  get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" }) # Form for new entry
+  post("/places/:place_id/entries/new", { :controller => "entries", :action => "create" }) # Process new entry
 end
